51585147c1cfe8584ff01e8d4e3b5ad1808fe081
DDExpo <f8ck.th1sm3n@gmail.com>
2024-08-12 04:01:20 +0600

    commit need for testing #4

--- tests/commands_test/test_write_tree.py
+++ tests/commands_test/test_write_tree.py
@@ -10,12 +10,13 @@ def setup_data():
 
     base_path = Path(__file__).parent.parent.parent
 
-    return [(base_path, "d5162deab68ae9a41d87cc1152f20a7e494536c6")]
+    return [(base_path, "fd5395778e0449c16b30f4e0e772066fd3fa6629")]
 
 
 def test_gyat_write_tree(setup_data):
     '''
-    Verifies `gyat_write_tree' if func return corect hash of tree.
+    Verifies `gyat_write_tree' if func return corect
+    hash of tree object from the current index.
     '''
 
     for base_path, answer in setup_data:
