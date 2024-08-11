ce5d3637ab7f1ee0ff40af812b8733047574015a
DDExpo <f8ck.th1sm3n@gmail.com>
2024-08-12 04:18:48 +0600

    commit need for testing #5

--- tests/commands_test/test_write_tree.py
+++ tests/commands_test/test_write_tree.py
@@ -3,12 +3,13 @@ from pathlib import Path
 import pytest
 
 from MyGyat.commands import gyat_write_tree
+from MyGyat.utils_utils import find_repo_gyat
 
 
 @pytest.fixture
 def setup_data():
 
-    base_path = Path(__file__).parent.parent.parent
+    base_path = find_repo_gyat()
 
     return [(base_path, "fd5395778e0449c16b30f4e0e772066fd3fa6629")]
 
@@ -19,5 +20,17 @@ def test_gyat_write_tree(setup_data):
     hash of tree object from the current index.
     '''
 
+    # So write_tree depends on current stage of index so sha of tree would
+    # change with changes to this file so to test this func it will be ignored
+    # while testing and of course it was in gitignore when creating sha
+    # with git to get valid answer to compare with func result
+    open(setup_data[0][0] / ".gitignore", "a",
+         encoding="utf-8").write(Path(__file__))
+
     for base_path, answer in setup_data:
         assert gyat_write_tree(base_dir=base_path) == answer
+
+    with open(setup_data[0][0] / ".gitignore", "a") as gignore:
+        lines = gignore.readlines()
+        lines.pop()
+        gignore.writelines(lines)
